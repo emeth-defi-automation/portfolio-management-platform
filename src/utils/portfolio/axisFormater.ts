dc865ef0611288495d912174a78c95be0699c791
Yberjon <nunnom1@gmail.com>
2024-04-17 12:31:49 +0200

    Spelling fix

--- src/utils/portfolio/axisFormater.ts
+++ src/utils/portfolio/axisFormater.ts
@@ -3,21 +3,21 @@ import { getSelectedPeriodKey } from "../timestamps/timestamp";
 import * as d3 from "d3";
 
 export const axisFormater = (value: Date, period: PeriodState): string => {
-  let formater = null;
+  let formatter = null;
   const key = getSelectedPeriodKey(period);
   switch (key) {
     case "24h":
-      formater = d3.utcFormat("%a %H:%M");
-      return formater(value as Date);
+      formatter = d3.utcFormat("%a %H:%M");
+      return formatter(value as Date);
     case "1W":
-      formater = d3.utcFormat("%a %d.%m");
-      return formater(value as Date);
+      formatter = d3.utcFormat("%a %d.%m");
+      return formatter(value as Date);
     case "1M":
-      formater = d3.utcFormat("%d.%m");
-      return formater(value as Date);
+      formatter = d3.utcFormat("%d.%m");
+      return formatter(value as Date);
     case "1Y":
-      formater = d3.utcFormat("%Y %b");
-      return formater(value as Date);
+      formatter = d3.utcFormat("%Y %b");
+      return formatter(value as Date);
   }
   return "";
 };
