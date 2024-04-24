6e75f7968875d38bc7c57974ff0d6ed1554c6345
pgajek <pgajek94@gmail.com>
2024-04-24 19:40:31 +0200

    button exmaple with args

--- src/components/Buttons/Buttons.stories.tsx
+++ src/components/Buttons/Buttons.stories.tsx
@@ -1,39 +1,52 @@
 import type { Meta, StoryObj } from "storybook-framework-qwik";
 import { ConnectButton, type ButtonProps } from "./Buttons";
 
-const meta: Meta<ButtonProps> = {
-  component: ConnectButton,
-};
+export default {
+  title: 'atoms/Button',
+  component: ConnectButton
+}
 
-type Story = StoryObj<ButtonProps>;
+export function Default(args: ButtonProps) {
+  return <ConnectButton  
+  {...args}
+      
+       />
+}
 
-export default meta;
+export function WalletConnect(args: ButtonProps) {
+  return <ConnectButton  
+  {...args}
+       />
+}
 
-export const WalletConnect: Story = {
-  render: (props) => (
-    <ConnectButton
-      {...props}
-      image="/assets/icons/login/walletconnect.svg"
-      text="Use WalletConnect"
-    ></ConnectButton>
-  ),
-};
+export function Info(args: ButtonProps) {
+  return <ConnectButton  
+  {...args}
+       />
+}
 
-export const Metamask: Story = {
-  render: () => (
-    <ConnectButton
-      image="/assets/icons/login/metamask.svg"
-      text="Use Metamask"
-    ></ConnectButton>
-  ),
-};
+Default.args = {
+  image:"/assets/icons/login/metamask.svg",
+  text:"Use Metamask"
+}
 
-export const Info: Story = {
-  render: () => (
-    <ConnectButton
-      image="/assets/icons/info-white.svg"
-      text="How to use Wallet?"
-      class="w-52 !border-0 bg-customBlue py-2 pl-2 pr-3 text-xs"
-    />
-  ),
-};
+WalletConnect.args = {
+       image:"/assets/icons/login/walletconnect.svg",
+      text:"Use WalletConnect"
+}
+
+Info.args = {
+      image:"/assets/icons/info-white.svg",
+       text:"How to use Wallet?",
+       class:"w-52 !border-0 bg-customBlue py-2 pl-2 pr-3 text-xs"
+}
+
+
+
+
+     
+
+
+
+
+   
