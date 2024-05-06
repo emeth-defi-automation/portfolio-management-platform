58b8e052adee1905a9a47b3bde3724defd622888
Marcin Łesek <daav95@gmail.com>
2024-05-07 00:36:55 +0200

    feat: setup synpress basic test

---
+++ tests/specs/sing-in-with-synpress.spec.ts
@@ -0,0 +1,33 @@
+import { MetaMask } from '@synthetixio/synpress';
+import basicSetup from '../config/wallet/basic.setup';
+import DashboardPage from '../pages/app/dashboard.page';
+import LoginPage from '../pages/login.page';
+import SignInPage from '../pages/signin.page';
+import { test } from '../util/test-synpress';
+
+test.describe('sign in', () => {
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+  });
+
+  test.only("should allow user to connect wallet with 'Use Metamask' flow", async ({ context, page, extensionId }) => {
+    const metamask = new MetaMask(context, page, basicSetup.walletPassword, extensionId);
+
+    const loginPage = new LoginPage(page);
+    const signInPage = new SignInPage(page);
+    const dashboardPage = new DashboardPage(page);
+
+    await loginPage.verifyTitle();
+    await loginPage.useMetamask();
+
+    await signInPage.useMetamaskWithConnectWalletModal();
+    await metamask.connectToDapp();
+    await signInPage.acceptAndSign();
+
+    await metamask.confirmSignature();
+
+    await dashboardPage.verifyPortfolioValue("0.00");
+  });
+});
