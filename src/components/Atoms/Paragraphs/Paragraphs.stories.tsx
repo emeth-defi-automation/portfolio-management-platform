ff28a3ee3473212836368862682aaf128ae149c3
DariaKozlovska <odriy.lovska@gmail.com>
2024-05-06 17:35:14 +0200

    atomic-design-paragraphs

--- src/components/Atoms/Paragraphs/Paragraphs.stories.tsx
+++ src/components/Atoms/Paragraphs/Paragraphs.stories.tsx
@@ -6,43 +6,42 @@ export function PrimaryParagraph(args: ParagraphProps) {
   return <Paragraph {...args} />;
 }
 
+export function MediumParagraph(args: ParagraphProps) {
+  return <Paragraph {...args} />;
+}
+
 export function SecondaryParagraph(args: ParagraphProps) {
   return <Paragraph {...args} />;
 }
 
-export function GreenParagraph(args: ParagraphProps) {
+export function GradientParagraph(args: ParagraphProps) {
   return <Paragraph {...args} />;
 }
 
-export function GradientParagraph(args: ParagraphProps) {
-    return <Paragraph {...args} />;
-  }
-
 PrimaryParagraph.args = {
-    text: "The connection attempt failed. Please click try again and follow the steps to connect in your wallet.",
-    variant: "primaryText",
-    size: "base",
-    weight: "regular"
+  text: "The connection attempt failed. Please click try again and follow the steps to connect in your wallet.",
+  variant: "primaryText",
+  size: "base",
+  weight: "regular",
+};
+
+MediumParagraph.args = {
+  text: "Bitcoin share exceeded 20%",
+  variant: "primaryText",
+  size: "sm",
+  weight: "medium",
 };
 
 SecondaryParagraph.args = {
-    text: "6 hours ago",
-    variant: "secondaryText",
-    size: "xs",
-    weight: "regular"
-};
-
-GreenParagraph.args = {
-    text: "+3,36%",
-    variant: "greenText",
-    size: "xs",
-    weight: "regular"
+  text: "Take actions in order to receive first alerts",
+  variant: "secondaryText",
+  size: "xs",
+  weight: "regular",
 };
 
 GradientParagraph.args = {
-    text: "$32 311,00",
-    variant: "gradientText",
-    size: "xl",
-    weight: "semiBold",
-    class: ""
+  text: "$32 311,00",
+  variant: "gradientText",
+  size: "xl",
+  weight: "semiBold",
 };
--- src/components/Atoms/Paragraphs/Paragraphs.tsx
+++ src/components/Atoms/Paragraphs/Paragraphs.tsx
@@ -9,39 +9,41 @@ export interface ParagraphProps {
 const ParagraphStyles = cva(["font-['Sora']"], {
   variants: {
     variant: {
-        primaryText: ['color-white'],
-        secondaryText: ['custom-text-50'],
-        greenText: ['color-customGreen'],
-        gradientText: ['custom-text-gradient text-transparent'],
+      primaryText: ["color-white"],
+      secondaryText: ["custom-text-50"],
+      greenText: ["color-customGreen"],
+      gradientText: ["custom-text-gradient text-transparent"],
     },
     size: {
-        xs: ['text-xs'],
-        sm: ['text-sm'],
-        base: ['text-base'],
-        xl: ['text-xl'],
+      xs: ["text-xs"],
+      sm: ["text-sm"],
+      base: ["text-base"],
+      xl: ["text-xl"],
     },
     weight: {
-        light: ['font-light'],
-        regular: ['font-normal'],
-        semiBold: ['font-semibold'],
+      light: ["font-light"],
+      regular: ["font-normal"],
+      medium: ["font-medium"],
+      semiBold: ["font-semibold"],
     },
     defaultVariant: {
-        variant: "primaryText",
-        size: "sm",
-        weight: "regular",
+      variant: "primaryText",
+      size: "sm",
+      weight: "regular",
     },
   },
 });
 
-export type ParagraphType = VariantProps<typeof ParagraphStyles> & ParagraphProps;
+export type ParagraphType = VariantProps<typeof ParagraphStyles> &
+  ParagraphProps;
 
 const Paragraph = ({ variant, size, weight, ...props }: ParagraphType) => {
   return (
     <p
-    {...props}
-    class={twMerge(ParagraphStyles({ variant, size, weight }), props.class)}
-    >{props.text}
-    <span class="hidden px-3"></span>
+      {...props}
+      class={twMerge(ParagraphStyles({ variant, size, weight }), props.class)}
+    >
+      {props.text}
     </p>
   );
 };
