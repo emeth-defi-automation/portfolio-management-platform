4f4376011f80757d05ce6cfab42274cbd3935630
Uacias <uaciasdev@gmail.com>
2024-05-08 09:45:22 +0200

    Add more cases to swap
    Added weth -> erc20 and erc20 -> weth. This should cover all 3 cases.
    Add token query to get decimals in separate file.

--- src/database/tokens.ts
+++ src/database/tokens.ts
@@ -1,4 +1,5 @@
-import { server$ } from "@builder.io/qwik-city";
+import { server$, z } from "@builder.io/qwik-city";
+import { type Surreal } from "surrealdb.js";
 import { connectToDB } from "~/database/db";
 import { type Token } from "~/interface/token/Token";
 
@@ -15,3 +16,22 @@ export const fetchTokens = server$(async function () {
   const db = await connectToDB(this.env);
   return await db.select<Token>("token");
 });
+
+
+export const GetTokenDecimalsResult = z.object({
+  decimals: z.string(),
+});
+
+export type GetTokenDecimalsResult = z.infer<typeof GetTokenDecimalsResult>;
+
+export const getTokenDecimals = async (db: Surreal, address: string) => {
+  try {
+
+    const queryResult = (await db.query(`SELECT decimals FROM token WHERE address = ${address}`)).at(0);
+    console.log("tokenDecimals", queryResult);
+    return GetTokenDecimalsResult.parse(queryResult);
+  } catch (e) {
+    console.error("Error in getTokenDecimals: ", e);
+    throw e;
+  }
+}
--- src/utils/tokens/swap.tsx
+++ src/utils/tokens/swap.tsx
@@ -2,8 +2,8 @@ import { server$ } from "@builder.io/qwik-city";
 import { readContract, writeContract } from "@wagmi/core";
 import { simulateContract } from "viem/actions";
 import { uniswapRouterAbi } from "~/abi/UniswapRouterAbi";
-import { type WagmiConfig } from "~/components/WalletConnect/context";
 import { connectToDB } from "~/database/db";
+import { getTokenDecimals } from "~/database/tokens";
 
 export const swapTokens = server$(async function (
   firstTokenAddress: string,
@@ -13,17 +13,18 @@ export const swapTokens = server$(async function (
   accountAddress: string,
   wagmiConfig: any,
 ) {
-  // get deadline
   const deadline = BigInt(Math.floor(Date.now() / 1000) + 200000);
-  // get decimals of token1
-  // TODO: Move to separate file and validate it
-  //   const db = await connectToDB(this.env);
-  //   const tokenDecimals = await db.query(
-  //     `SELECT VALUE decimals FROM token WHERE address = ${firstTokenAddress}`,
-  //   );
-  const amountIn = BigInt(parseFloat(amount) * 10 ** 18);
-  // get amountIn = amount * 10^decimals
   try {
+    const wrappedEtherAddress = await readContract(wagmiConfig.config, {
+      abi: uniswapRouterAbi,
+      address: routerContractAddress as `0x${string}`,
+      functionName: "WETH",
+    });
+    const db = await connectToDB(this.env);
+    const tokenDecimals = await getTokenDecimals(db, firstTokenAddress);
+    const amountIn = BigInt(
+      parseFloat(amount) * 10 ** parseInt(tokenDecimals.decimals),
+    );
     const amountOutMin = await readContract(wagmiConfig.config, {
       abi: uniswapRouterAbi,
       address: routerContractAddress as `0x${string}`,
@@ -36,23 +37,58 @@ export const swapTokens = server$(async function (
         ],
       ],
     });
-
-    const { request } = await simulateContract(wagmiConfig.config, {
-      abi: uniswapRouterAbi,
-      address: "0x87aE49902B749588c15c5FE2A6fE6a1067a5bea0",
-      functionName: "swapExactTokensForTokens",
-      args: [
-        amountIn,
-        amountOutMin[0],
-        [
-          firstTokenAddress as `0x${string}`,
-          secondTokenAddress as `0x${string}`,
+    if (firstTokenAddress === wrappedEtherAddress) {
+      const { request } = await simulateContract(wagmiConfig.config, {
+        abi: uniswapRouterAbi,
+        address: "0x87aE49902B749588c15c5FE2A6fE6a1067a5bea0",
+        functionName: "swapExactETHForTokens",
+        args: [
+          amountOutMin[0],
+          [
+            firstTokenAddress as `0x${string}`,
+            secondTokenAddress as `0x${string}`,
+          ],
+          accountAddress as `0x${string}`,
+          deadline,
         ],
-        accountAddress as `0x${string}`,
-        deadline,
-      ],
-    });
-    await writeContract(wagmiConfig.config, request);
+        value: amountIn,
+      });
+      await writeContract(wagmiConfig.config, request);
+    } else if (secondTokenAddress === wrappedEtherAddress) {
+      const { request } = await simulateContract(wagmiConfig.config, {
+        abi: uniswapRouterAbi,
+        address: "0x87aE49902B749588c15c5FE2A6fE6a1067a5bea0",
+        functionName: "swapExactTokensForETH",
+        args: [
+          amountIn,
+          amountOutMin[0],
+          [
+            firstTokenAddress as `0x${string}`,
+            secondTokenAddress as `0x${string}`,
+          ],
+          accountAddress as `0x${string}`,
+          deadline,
+        ],
+      });
+      await writeContract(wagmiConfig.config, request);
+    } else {
+      const { request } = await simulateContract(wagmiConfig.config, {
+        abi: uniswapRouterAbi,
+        address: "0x87aE49902B749588c15c5FE2A6fE6a1067a5bea0",
+        functionName: "swapExactTokensForTokens",
+        args: [
+          amountIn,
+          amountOutMin[0],
+          [
+            firstTokenAddress as `0x${string}`,
+            secondTokenAddress as `0x${string}`,
+          ],
+          accountAddress as `0x${string}`,
+          deadline,
+        ],
+      });
+      await writeContract(wagmiConfig.config, request);
+    }
   } catch (e) {
     console.error("Error in swap: ", e);
   }
