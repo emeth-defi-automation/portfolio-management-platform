0affdc990585b208e50240465c26531ded998e56
Anhelina Kruk <krukanhelina06@gmail.com>
2024-05-10 13:51:21 +0200

    fixing NoData molecule

--- src/components/Molecules/NoData/NoData.stories.tsx
+++ src/components/Molecules/NoData/NoData.stories.tsx
@@ -1,15 +1,15 @@
-import NoDataBlock, { type NoDataProps } from "./NoData";
+import NoData, { type NoDataProps } from "./NoData";
 
 export default {
-  title: "atoms/Box",
-  component: NoDataBlock,
+  title: "molecules/Nodata",
+  component: NoData,
 };
 
-export function NoData(args: NoDataProps) {
-  return <NoDataBlock {...args} />;
+export function NoDataBlock(args: NoDataProps) {
+  return <NoData {...args} />;
 }
 
-NoData.args = {
+NoDataBlock.args = {
   title: "No data available yet",
   description: "To view the chart, set up your wallets and deposit funds.",
 };
--- src/components/Molecules/NoData/NoData.tsx
+++ src/components/Molecules/NoData/NoData.tsx
@@ -1,6 +1,10 @@
 import { twMerge } from "tailwind-merge";
 import IconInfoWhite from "/public/assets/icons/info-white.svg?jsx";
 import Button from "~/components/Atoms/Buttons/Button";
+import Header from "~/components/Atoms/Headers/Header";
+import Paragraph from "~/components/Atoms/Paragraphs/Paragraphs";
+import { cva, type VariantProps  } from "class-variance-authority";
+// import { Slot } from "@builder.io/qwik";
 
 export interface NoDataProps {
   title: string;
@@ -8,24 +12,43 @@ export interface NoDataProps {
   class?: string;
 }
 
-export type tagType = NoDataProps;
+// TODO: different icons variants
+const NoDataStyles = cva(
+  [
+    "",
+  ],
+  {
+    variants: {
+      variant: {
+        info: [""],
+        warning: [""],
+        success: [""]
+      },
+    },
+    defaultVariants: {
+      variant: "info",
+    },
+  },
+);
 
-const NoDataBlock = ({ ...props }) => {
+export type NoDataType = VariantProps<typeof NoDataStyles> & NoDataProps;
+
+const NoData = ({ ...props }) => {
   return (
     <div {...props} class={twMerge("flex flex-col gap-4", props.class)}>
       <div class="flex flex-col items-center justify-center gap-4">
         <IconInfoWhite class="h-10 w-10" />
-        {/* TODO: change h3 and p to atoms */}
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+        <Header variant="h5" text={props.title}/>
+        <Paragraph variant="secondaryText" size="xs" weight="regular" text={props.description}/>
       </div>
 
       <div class="flex items-center justify-center gap-2">
         <Button variant="transparent" text="Deposit Funds" class="py-2.5" />
         <Button text="Setup Wallet" class="py-2.5" />
+        {/* <Slot/> */}
       </div>
     </div>
   );
 };
 
-export default NoDataBlock;
+export default NoData;
