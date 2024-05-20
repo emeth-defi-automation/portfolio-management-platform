049fd50bfa5cd75a38479e152917ea1d8046df4a
Anhelina <krukanhelina06@gmail.com>
2024-05-20 22:40:27 +0200

    fix checkbox & grid

--- src/routes/app/automation/index.tsx
+++ src/routes/app/automation/index.tsx
@@ -2,19 +2,23 @@ import { component$ } from "@builder.io/qwik";
 import Button from "~/components/Atoms/Buttons/Button";
 import { ButtonWithIcon } from "~/components/Buttons/Buttons";
 import IconEdit from "@material-design-icons/svg/outlined/edit.svg?jsx";
-import IconToggleOn from "@material-design-icons/svg/filled/toggle_on.svg?jsx";
-// import Checkbox from "~/components/Atoms/Checkbox/Checkbox";
+import Checkbox from "~/components/Atoms/Checkbox/Checkbox";
 import Header from "~/components/Atoms/Headers/Header";
 import Paragraph from "~/components/Atoms/Paragraphs/Paragraphs";
 
 export default component$(() => {
   return (
     <>
-      <div class="grid grid-cols-[30%_70%]">
-        <div class="border-r border-white/10 bg-white/[0.03] grid grid-rows-[32px_40px_1fr] gap-6 p-6">
+      <div class="grid grid-cols-[1fr_2.5fr]">
+        <div class="grid grid-rows-[32px_40px_1fr] gap-6 border-r border-white/10 bg-white/[0.03] p-6">
           <div class="flex items-center justify-between gap-2">
-            <Header variant="h4" text="Automations" class="font-normal"/>
-            <Button text="Add New" variant="transparent" size="small" customClass="font-normal bg-white/10 !border-0" />
+            <Header variant="h4" text="Automations" class="font-normal" />
+            <Button
+              text="Add New"
+              variant="transparent"
+              size="small"
+              customClass="font-normal bg-white/10 !border-0"
+            />
           </div>
           <div class="grid w-full gap-2">
             <ButtonWithIcon
@@ -24,41 +28,43 @@ export default component$(() => {
             />
           </div>
           <div class="">
-              <div class="p-4 flex items-center justify-between gap-12">
-                <div class="flex flex-col gap-3">
-                  <Header variant="h5" text="Automation Name #1"/>
-                  <Paragraph size="xs" variant="secondaryText" text="At a scheduled time with a defined alert"/>
-                </div>
-                <div>
-                  <IconToggleOn class="w-10 h-10 fill-customGreen"/>
-                </div>
+            <div class="flex items-center justify-between gap-12 p-4">
+              <div class="flex flex-col gap-3">
+                <Header variant="h5" text="Automation Name #1" />
+                <Paragraph
+                  size="xs"
+                  variant="secondaryText"
+                  text="At a scheduled time with a defined alert"
+                />
               </div>
-              <div class="p-4 flex items-center justify-between gap-12">
-                <div class="flex flex-col gap-3">
-                  <Header variant="h5" text="Automation Name #2"/>
-                  <Paragraph size="xs" variant="secondaryText" text="At a scheduled time with a defined alert"/>
-                </div>
-                <div>
-                  <IconToggleOn class="w-10 h-10 fill-customGreen"/>
-                </div>
+              <Checkbox variant="toggleTick" checked={true} class="" />
+            </div>
+            <div class="flex items-center justify-between p-4">
+              <div class="flex flex-col gap-3">
+                <Header variant="h5" text="Automation Name #2" />
+                <Paragraph
+                  size="xs"
+                  variant="secondaryText"
+                  text="At a scheduled time with a defined alert"
+                />
               </div>
+              <Checkbox variant="toggleTick" checked={true} />
+            </div>
           </div>
         </div>
         <div class="p-6">
-          <div class="flex justify-between items-center">
+          <div class="flex items-center justify-between">
             <div class="flex gap-2">
-              <Header variant="h4" text="New Automation" class="font-normal"/>
-              <IconEdit class="w-4 h-4 fill-white"/>
+              <Header variant="h4" text="New Automation" class="font-normal" />
+              <Button
+                variant="onlyIcon"
+                leftIcon={<IconEdit class="h-4 w-4 fill-white" />}
+              />
             </div>
             <div class="flex items-center gap-2">
-              <Paragraph size="xs" class="text-customGreen" text="Active"/>
-              <IconToggleOn class="w-10 h-10 fill-customGreen"/>
+              <Paragraph size="xs" class="text-customGreen" text="Active" />
+              <Checkbox variant="toggleTick" checked={true} class="" />
             </div>
-            {/* <Checkbox 
-              variant="toggleTick"
-              checked={true}
-              class=""
-            /> */}
           </div>
         </div>
       </div>
