7f62cc2fd716b961e715befbe8dac91fd930952d
furmanp <furmanp@proton.me>
2024-05-28 20:59:16 +0200

    - added modal to confirm logging out

--- src/components/Navbar/NavbarContent.tsx
+++ src/components/Navbar/NavbarContent.tsx
@@ -11,12 +11,14 @@ import { NavLink } from "./NavLink";
 import { useNavigate } from "@builder.io/qwik-city";
 import { disconnectWallets } from "~/utils/walletConnections";
 import Button from "~/components/Atoms/Buttons/Button";
+import { Modal } from "~/components/Modal/Modal";
 
 export const NavbarContent = component$(() => {
   const nav = useNavigate();
   const login = useContext(LoginContext);
   const wagmiConfig = useContext(WagmiConfigContext);
   const address = useSignal("");
+  const isLogoutModalOpen = useSignal<boolean>(false);
   // eslint-disable-next-line qwik/no-use-visible-task
   useVisibleTask$(({ track }) => {
     track(() => login.address.value);
@@ -51,18 +53,42 @@ export const NavbarContent = component$(() => {
           variant="onlyIcon"
           leftIcon={<IconLogout class="h-6 w-6 fill-white" />}
           onClick$={async () => {
-            localStorage.removeItem("refreshToken");
-            document.cookie =
-              "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/app;";
-
-            login.account = undefined;
-            login.address.value = undefined;
-            login.chainId.value = undefined;
-            await disconnectWallets(wagmiConfig.config, true);
-            await nav("/");
+            console.log("logout");
+            isLogoutModalOpen.value = true;
           }}
         />
       </div>
+      {isLogoutModalOpen.value ? (
+        <Modal title="Confirm Logout" isOpen={isLogoutModalOpen}>
+          <div class="flex gap-4">
+            <Button
+              text="Logout"
+              onClick$={async () => {
+                isLogoutModalOpen.value = true;
+                localStorage.removeItem("refreshToken");
+                document.cookie =
+                  "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/app;";
+
+                login.account = undefined;
+                login.address.value = undefined;
+                login.chainId.value = undefined;
+                await disconnectWallets(wagmiConfig.config, true);
+                await nav("/");
+              }}
+              customClass="w-full"
+            />
+            <Button
+              text="Cancel"
+              type="button"
+              variant="transparent"
+              onClick$={() => {
+                isLogoutModalOpen.value = false;
+              }}
+              customClass="w-full"
+            />
+          </div>
+        </Modal>
+      ) : null}
     </>
   );
 });
