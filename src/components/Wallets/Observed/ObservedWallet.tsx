d18d53bfb6d0d83113c5f19b34f5e8590ab48122
Gawc1uuu <gawlikjakub9@gmail.com>
2024-06-14 12:58:32 +0200

    adding live query for wallet name and formating code

--- src/components/Wallets/Observed/ObservedWallet.tsx
+++ src/components/Wallets/Observed/ObservedWallet.tsx
@@ -1,4 +1,4 @@
-import { type Signal, component$, useContext, useSignal } from "@builder.io/qwik";
+import { type Signal, component$, useContext, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import { type WalletTokensBalances } from "~/interface/walletsTokensBalances/walletsTokensBalances";
 import IconEthereum from "/public/assets/icons/ethereum.svg?jsx";
 import IconClock from "/public/assets/icons/wallets/clock.svg?jsx";
@@ -6,14 +6,122 @@ import {
   SelectedWalletDetailsContext,
   SelectedWalletNameContext,
 } from "~/routes/app/wallets";
+import { server$ } from "@builder.io/qwik-city";
+import { connectToDB } from "~/database/db";
+import { Readable } from "stream";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { killLiveQuery } from "~/components/ObservedWalletsList/ObservedWalletsList";
 
 interface ObservedWalletProps {
   observedWallet: any;
   chainIdToNetworkName: { [key: string]: string };
 }
 
+
+export const fetchObservedWalletName = server$(async function (
+  observedWalletId: string
+) {
+  const db = await connectToDB(this.env);
+  const [[observedWalletName]]: any = await db.query(
+    `SELECT VALUE name FROM observes_wallet WHERE out = ${observedWalletId}`
+  );
+
+  return observedWalletName;
+
+})
+
+export const observedWalletNameLiveStream = server$(async function* (walletId: string) {
+  const db = await connectToDB(this.env)
+
+  const resultStream = new Readable({
+    objectMode: true,
+    read() { },
+  });
+
+  const cookie = this.cookie.get("accessToken")?.value;
+  if (!cookie) {
+    throw new Error("No cookie found");
+  }
+  const { userId } = jwt.decode(cookie) as JwtPayload
+
+  const queryUuid: any = await db.query(`
+    LIVE SELECT name FROM observes_wallet WHERE in=${userId} and out=${walletId}
+    `)
+
+
+  yield queryUuid[0];
+
+  await db.query(`
+    INSERT INTO queryuuids (queryuuid, enabled) VALUES ('${queryUuid[0]}',${true});
+    `);
+
+
+  const queryUuidEnabledLive: any = await db.query(`
+    LIVE SELECT enabled FROM queryuuids WHERE queryuuid = '${queryUuid[0]}';
+    `);
+
+  await db.listenLive(queryUuidEnabledLive[0],
+    ({ action }) => {
+      if (action === "UPDATE") {
+        resultStream.push(null);
+        db.kill(queryUuidEnabledLive[0]);
+      }
+    });
+
+
+  const observedWalletName = (await fetchObservedWalletName(walletId));
+
+  yield observedWalletName;
+
+  await db.listenLive(
+    queryUuid[0],
+    async ({ action, result }) => {
+      if (action === "CLOSE") {
+        resultStream.push(null);
+        await db.kill(queryUuid[0])
+        return;
+      }
+      resultStream.push({ action, result })
+    }
+  );
+
+  for await (const result of resultStream) {
+    if (!result) {
+      break;
+    }
+    yield result;
+  }
+
+})
+
+
 export const ObservedWallet = component$<ObservedWalletProps>(
   ({ observedWallet, chainIdToNetworkName }) => {
+
+    const observedWalletNameSignal = useSignal("Loading name...");
+    // eslint-disable-next-line qwik/no-use-visible-task
+    useVisibleTask$(async ({ cleanup }) => {
+      cleanup(async () => {
+        await killLiveQuery(queryUuid.value);
+      });
+
+      if (!observedWallet.id) throw new Error("observedWallet.id is undefined");
+
+      const data = await observedWalletNameLiveStream(observedWallet.id);
+      const queryUuid = await data.next();
+
+      observedWalletNameSignal.value = (await data.next()).value;
+
+      for await (const value of data) {
+        if (value.action === "CREATE" || value.action === "UPDATE") {
+          observedWalletNameSignal.value = value.result.name;
+        } else if (value.action === "DELETE") {
+          observedWalletNameSignal.value = "";
+        }
+      }
+
+
+    })
 
     const selectedWalletDetails = useContext(SelectedWalletDetailsContext);
     const observedWalletNameContext = useContext(SelectedWalletNameContext);
@@ -32,7 +140,7 @@ export const ObservedWallet = component$<ObservedWalletProps>(
             <IconEthereum />
           </div>
           <div class="">
-            <div class="text-sm">{observedWallet.id}</div>
+            <div class="text-sm">{observedWalletNameSignal.value}</div>
             <div class="custom-text-50 text-xs">
               {chainIdToNetworkName[observedWallet.chainId]}
             </div>
