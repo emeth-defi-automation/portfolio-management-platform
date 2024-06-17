60e608ee4ed46425eec378e995371e52dcc16801
Anhelina Kruk <krukanhelina06@gmail.com>
2024-06-17 10:24:01 +0200

    created DetailsBox atom

--- src/components/Atoms/DetailsBox/DetailsBox.tsx
+++ src/components/Atoms/DetailsBox/DetailsBox.tsx
@@ -1,39 +1,50 @@
-
 import { component$ } from "@builder.io/qwik";
-import { cva, type VariantProps } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
+import Annotation from "../Annotation/Annotation";
+import IconOpen from "@material-design-icons/svg/filled/open_in_new.svg?jsx";
+import Button from "../Buttons/Button";
 
 export interface DetailsBoxProps {
   customClass: string;
+  title?: string;
+  text?: string;
+  tokenPath?: string;
+  isAddress?: boolean;
 }
 
-const DetailsBoxStyles = cva(
-  ["flex justify-between items-center"],
-  {
-    variants: {
-      variant: {
-        
-      },
-    },
-    defaultVariants: {
-      
-    },
-  },
-);
-
-export type DetailsBoxType = VariantProps<typeof DetailsBoxStyles> & DetailsBoxProps;
-
-const DetailsBox = component$(({...props }: DetailsBoxType) => {
+export const DetailsBox = component$<DetailsBoxProps>((props) => {
   return (
-    <>
-      <div
-        {...props}
-        class={twMerge(DetailsBoxStyles({ }), props.customClass)}
-      >
-        
+    <div
+      class={twMerge(
+        "flex items-center justify-between rounded-lg bg-black/10 p-4",
+        props.customClass,
+      )}
+    >
+      <div class="flex items-center gap-2">
+        {props.tokenPath ? (
+          <img width={16} height={16} src={props.tokenPath} />
+        ) : (
+          ""
+        )}
+        <Annotation
+          text={props.title}
+          variant={props.tokenPath ? "white" : "default"}
+        />
       </div>
-    </>
+      <div class="flex items-center gap-2">
+        <Annotation
+          text={props.text}
+          variant={props.tokenPath ? "default" : "white"}
+        />
+        {props.isAddress ? (
+          <Button
+            variant="onlyIcon"
+            leftIcon={<IconOpen class="h-3 w-3 fill-white" />}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
   );
 });
-
-export default DetailsBox;
