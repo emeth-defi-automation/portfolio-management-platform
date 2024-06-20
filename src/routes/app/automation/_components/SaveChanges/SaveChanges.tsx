3d878ce6c912a329cd513f49089a43456a6422f8
Anhelina Kruk <krukanhelina06@gmail.com>
2024-06-20 15:48:29 +0200

    fmt

--- src/routes/app/automation/_components/SaveChanges/SaveChanges.tsx
+++ src/routes/app/automation/_components/SaveChanges/SaveChanges.tsx
@@ -3,24 +3,18 @@ import Button from "~/components/Atoms/Buttons/Button";
 import IconError from "@material-design-icons/svg/filled/error_outline.svg?jsx";
 import Paragraph from "~/components/Atoms/Paragraphs/Paragraphs";
 
-export const SaveChanges = component$(
-  () => {
-    return (
-      <div class="relative bottom-0 flex items-center w-full justify-between h-20 p-6 border-t border-white/10 bg-white/3">
-        <div class="flex items-center gap-2">
-          <IconError class="fill-white w-7 h-7"/>
-          <Paragraph
-            size="xs" 
-            class="max-w-[290px] text-wrap !leading-normal"
-            text="Remember, saving changes sends a transaction to the blockchain and incurs a gas fee."
-          />
-        </div>
-        <Button
-          size="small"
-          text="Save Changes"
-          customClass="font-normal"
+export const SaveChanges = component$(() => {
+  return (
+    <div class="relative bottom-0 flex h-20 w-full items-center justify-between border-t border-white/10 bg-white/3 p-6">
+      <div class="flex items-center gap-2">
+        <IconError class="h-7 w-7 fill-white" />
+        <Paragraph
+          size="xs"
+          class="max-w-[290px] text-wrap !leading-normal"
+          text="Remember, saving changes sends a transaction to the blockchain and incurs a gas fee."
         />
       </div>
-    );
-  },
-);
+      <Button size="small" text="Save Changes" customClass="font-normal" />
+    </div>
+  );
+});
