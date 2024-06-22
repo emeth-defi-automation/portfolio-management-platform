331a2b56cd2a3c20396759cc1ff8cd349cb90e2c
Anhelina Kruk <krukanhelina06@gmail.com>
2024-06-22 14:12:20 +0200

    fixing approval limit input

--- src/components/FormBadge/FormBadge2.stories.tsx
+++ src/components/FormBadge/FormBadge2.stories.tsx
@@ -1,5 +1,8 @@
+import { $ } from "@builder.io/qwik";
 import Checkbox from "../Atoms/Checkbox/Checkbox";
+import Input from "../Atoms/Input/Input";
 import { FormBadge2, type FormBadge2Props } from "./FormBadge2";
+import IconSuccess from "@material-design-icons/svg/round/check_circle_outline.svg?jsx";
 
 export default {
   component: FormBadge2,
@@ -14,7 +17,23 @@ export function SelectToken(args: FormBadge2Props) {
 }
 
 export function ApprovalLimit(args: FormBadge2Props) {
-  return <FormBadge2 {...args}></FormBadge2>;
+  const onInput = $((e: any) => {
+    const target = e.target;
+    const value = target.value;
+    console.log(value);
+    return value;
+  });
+
+  return (
+    <FormBadge2 {...args}>
+      <Input
+        id=""
+        onInput={onInput}
+        // variant={inputValue !== "" ? "checked" : null}
+        iconRight={<IconSuccess class="h-4 w-4" />}
+      />
+    </FormBadge2>
+  );
 }
 
 SelectToken.args = {
