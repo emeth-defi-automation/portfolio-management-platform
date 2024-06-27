9c4b1710ed1bbf252c471bbc676fb0f025ccc116
Anhelina Kruk <krukanhelina06@gmail.com>
2024-06-27 17:09:02 +0200

    fixed CR bugs

--- src/components/FormBadge/FormBadge.stories.tsx
+++ src/components/FormBadge/FormBadge.stories.tsx
@@ -1,9 +1,5 @@
-import { $ } from "@builder.io/qwik";
 import Checkbox from "../Atoms/Checkbox/Checkbox";
-import Input from "../Atoms/Input/Input";
 import { FormBadge, type FormBadgeProps } from "./FormBadge";
-import IconSuccess from "@material-design-icons/svg/round/check_circle_outline.svg?jsx";
-import { checkPattern } from "~/utils/fractions";
 
 export default {
   component: FormBadge,
@@ -17,38 +13,7 @@ export function SelectToken(args: FormBadgeProps) {
   );
 }
 
-export function ApprovalLimit(args: FormBadgeProps) {
-  const onInput = $((e: any) => {
-    const target = e.target;
-    const value = target.value;
-    console.log(value);
-    return value;
-  });
-
-  const isValid = $((value: string) => {
-    return (
-      checkPattern(value, /^\d*\.?\d*$/) && value !== "0" && value.length > 0
-    );
-  });
-
-  return (
-    <FormBadge {...args}>
-      <Input
-        id=""
-        isValid={true}
-        // iconRight={isValid function ? <IconSuccess class="h-4 w-4" /> : null}
-      />
-    </FormBadge>
-  );
-}
-
 SelectToken.args = {
-  tokenName: "Bitcoin",
-  tokenSymbol: "BTC",
-  tokenPath: "/assets/icons/tokens/btc.svg",
-};
-
-ApprovalLimit.args = {
   tokenName: "Bitcoin",
   tokenSymbol: "BTC",
   tokenPath: "/assets/icons/tokens/btc.svg",
--- src/routes/app/wallets/_components/AmountOfCoins.tsx
+++ src/routes/app/wallets/_components/AmountOfCoins.tsx
@@ -27,12 +27,14 @@ export default component$<AmountOfCoinsProps>(
             );
 
             const isValid = () => {
-              return (
-                checkPattern(coin!.amount, /^\d*\.?\d*$/) &&
-                coin!.amount != "0" &&
-                coin!.amount.length > 0 &&
-                coin!.amount[0] != "0"
-              );
+              if (coin && coin.amount) {
+                return (
+                  checkPattern(coin.amount, /^\d*\.?\d*$/) &&
+                  coin.amount != "0" &&
+                  coin.amount.length > 0 &&
+                  coin.amount[0] != "0"
+                );
+              }
             };
             return (
               <div
@@ -46,58 +48,62 @@ export default component$<AmountOfCoinsProps>(
                     tokenSymbol={symbol}
                     tokenPath={`/assets/icons/tokens/${symbol.toLowerCase()}.svg`}
                   >
-                    <Input
-                      name={`${symbol}Amount`}
-                      id={`${symbol}Amount`}
-                      placeholder="Approval limit..."
-                      value={
-                        coin!.amount === "0"
-                          ? "Approval limit..."
-                          : coin!.amount
-                      }
-                      onInput={$((e) => {
-                        const target = e.target as HTMLInputElement;
-                        const regex = /^\d*\.?\d*$/;
-                        target.value = replaceNonMatching(
-                          target.value,
-                          regex,
-                          "",
-                        );
-                        coin!.amount = target.value;
-                      })}
-                      InputClass="pr-10"
-                      isValid={isValid()}
-                      iconRight={
-                        isValid() ? <IconSuccess class="h-4 w-4" /> : null
-                      }
-                    />
-                  </FormBadge>
-                  <Button
-                    text="max"
-                    variant="blue"
-                    size="small"
-                    onClick$={async () => {
-                      const inputTokenValue =
-                        addWalletFormStore.coinsToApprove.find(
-                          (item) => item.symbol === symbol,
-                        );
-                      const chosenTokenBalance = walletTokenBalances.value.find(
-                        (item: any) => item.symbol === symbol,
-                      );
-                      const decimals = chosenTokenBalance.decimals;
-                      const calculation =
-                        BigInt(chosenTokenBalance.balance) /
-                        BigInt(10 ** decimals);
-                      const denominator = chosenTokenBalance.balance
-                        .toString()
-                        .substring(
-                          calculation.toString().length,
-                          chosenTokenBalance.balance.toString().length - 1,
-                        );
+                    <div class="flex items-center gap-3">
+                      <Input
+                        name={`${symbol}Amount`}
+                        id={`${symbol}Amount`}
+                        placeholder="Approval limit..."
+                        value={
+                          coin!.amount === "0"
+                            ? "Approval limit..."
+                            : coin!.amount
+                        }
+                        onInput={$((e) => {
+                          const target = e.target as HTMLInputElement;
+                          const regex = /^\d*\.?\d*$/;
+                          target.value = replaceNonMatching(
+                            target.value,
+                            regex,
+                            "",
+                          );
+                          coin!.amount = target.value;
+                        })}
+                        InputClass="pr-10"
+                        isValid={isValid()}
+                        iconRight={
+                          isValid() ? <IconSuccess class="h-4 w-4" /> : null
+                        }
+                      />
+                      <Button
+                        text="Max"
+                        variant="blue"
+                        customClass="!p-1 !rounded-md"
+                        size="small"
+                        onClick$={async () => {
+                          const inputTokenValue =
+                            addWalletFormStore.coinsToApprove.find(
+                              (item) => item.symbol === symbol,
+                            );
+                          const chosenTokenBalance =
+                            walletTokenBalances.value.find(
+                              (item: any) => item.symbol === symbol,
+                            );
+                          const decimals = chosenTokenBalance.decimals;
+                          const calculation =
+                            BigInt(chosenTokenBalance.balance) /
+                            BigInt(10 ** decimals);
+                          const denominator = chosenTokenBalance.balance
+                            .toString()
+                            .substring(
+                              calculation.toString().length,
+                              chosenTokenBalance.balance.toString().length - 1,
+                            );
 
-                      inputTokenValue!.amount = `${calculation}.${denominator}`;
-                    }}
-                  />
+                          inputTokenValue!.amount = `${calculation}.${denominator}`;
+                        }}
+                      />
+                    </div>
+                  </FormBadge>
                 </div>
                 <span class="block pb-1 text-xs text-white">
                   {!checkPattern(
